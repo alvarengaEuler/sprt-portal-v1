5928659ec32b32b60debb34c4b238bfc12435db9
Euler Alvarenga <euler.alvarenga.dev@gmail.com>
2024-04-25 14:46:16 -0300

    remove invalid html code

--- app/components/sections/hero-section.tsx
+++ app/components/sections/hero-section.tsx
@@ -17,7 +17,7 @@ export const HeroSection = () => {
         >
           <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-6xl lg:leading-normal font-bold">
             <span className="text-transparent bg-clip-text bg-gradient-to-r from-red-400 to-blue-500">
-              Hi, I&apos;m{" "}
+              Hi, I'm{" "}
             </span>
             <br></br>
             <TypeAnimation
